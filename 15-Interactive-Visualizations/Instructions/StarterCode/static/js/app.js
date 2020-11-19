34298ac27ad822881bef8b242368494da7bb5d38
Rob Pascarella <robpascarella@Robs-iMac.local>
2020-11-18 19:47:33 -0600

    I've really messed some stuff up --- committing here but going back a rev when I was at least getting a chart plotted'

--- 15-Interactive-Visualizations/Instructions/StarterCode/static/js/app.js
+++ 15-Interactive-Visualizations/Instructions/StarterCode/static/js/app.js
@@ -11,8 +11,6 @@ function optionChanged(sampleID) {
     // buildTable(sampleID);
 };
 
-
-
 function init() {
     var selector = d3.select('#selDataset');
     d3.json('../samples.json').then(function (data) {
@@ -23,8 +21,9 @@ function init() {
                 .property('value', name)
         });
     });
+
     buildPlot("940");
-    buildTable("940");
+    buildTable("940"); s
 }
 
 init();
@@ -33,37 +32,63 @@ function buildPlot(selectedID) {
     // pull in the data
     d3.json("../samples.json").then(function (data) {
         console.log(data);
+
         // so I see we have names, metadata, and samples...
 
         // pull in and parse data: looks like there are 153 ID's, each with the own list of OTU_ID's 
         // want to grab the sample ID, that will eventually be selected from dropdown
 
+        // I will be shaping the pulls/matching of data via the otu_id
+        // within metadata, this item is an integer 
+        // within names and samples, this item is a string
 
-        // something that I'm able to select later via dropdown selection (listener?)
-        // within the html this is the 'selDataset'
-        // var sample_ID = data.names[.. location... ]
-
-        var sample = data.samples.filter(sample => sample.id === selectedID)[0];
+        // once the user has made a selection, the dataset can be searched
+        // this line is the first return array that matches in the sample...
+        var sample = data.samples.filter(sample => sample.id === parseInt(selectedID))[0];
         console.log(sample);
 
-        //this will be the metadata plotted on page along with each ID in the Demographic Info area.
+        // so now, "sample" in line 47 gives me the dataset for that sample id number
+        // which includes otu_labels, otu_ids, and the sample_values 
+        // the demographic info I need will need another similar search and be in sample.metadata
 
-        // from line 28, once the sample_ID is known, the OTU_ID's can be sorted
-        //var OTU_ID_sorted = data.samples.otu_id ...
+        // data for charting.. 2nd try 
+
+        var otu_id_top10_inSample = sample.samples.otu_ids.slice(0, 9).reverse();
+        console.log(otu_id_top10_inSample)
+
+        var chart_labels = sample.otu_labels.map(d => d.otu_ids.toString()).slice(0, 9).reverse();
+        // var chart_labels = otuIdsTop10.map(item => "OTU ID " + item);
+        console.log(chart_labels);
+
+        //var sampleValuesTop10 = sampleFiltered[0].sample_values.slice(0, 10).reverse();
+        var sample_values_top10_inSample = sample.sample_values.slice(0, 10).reverse();
+        console.log(sample_values_top10_inSample);
 
 
         var data = [
             {
-                y: sample.otu_ids.map(d => d.toString()).slice(0, 9),
-                x: sample.sample_values.slice(0.9),
+                // tried to be clever here and pull in what I wanted all in one line, wasn't particularly working
+                // instead I went back out of this list and created variables to carry each x, y
+
+                // y: sample.otu_ids.map(d => d.otu_ids.toString()).slice(0, 9).reverse(),
+                y: otu_id_top10_inSample,
+                // this one should work..
+                x: sample_values_top10_inSample,
+                // x: sample.sample_values.slice(0, 9).reverse(),
                 type: 'bar',
                 orientation: 'h'
             }
+
         ];
 
-        var layout =
+        var layout = {
+            title: "Count by OTU ID",
+            xaxis: { title: "Count" },
+            yaxis: { title: "OTU ID" }
+        };
 
-            Plotly.newPlot("bar-plot", data, layout);
+
+        Plotly.newPlot("bar-plot", data, layout);
 
 
 
@@ -78,18 +103,18 @@ function buildTable(selectedID) {
         console.log(data);
 
         var metadata_info = data.metadata.filter(metadata => metadata.id.toString() === selectedID)[0];
-        //metadata was not pulling correctly as integer, converting toString..
+        //as stated in comments above, metadata was not pulling correctly as integer, converting toString..
         console.log(metadata_info);
         //this will be the metadata plotted on page along with each ID in the Demographic Info area.
 
         var metaDataTable = d3.select("#sample-metadata")
+
+
         // can convert json key-value pairs with Object.entries(returns array of key:value)
         // for a forEach.append the text for each key:value pairs 
 
     });
 }
-
-
 
 
 
